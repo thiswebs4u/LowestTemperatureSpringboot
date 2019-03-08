7aedc35a89bdc2560f4e4d9dcf5b8204b6c243aa
thiswebs4u <john.hart@perficient.com>
2019-03-07 21:26:28 -0700

    good night

--- src/main/java/com/infosys/controller/LowestTemperatureController.java
+++ src/main/java/com/infosys/controller/LowestTemperatureController.java
@@ -1,5 +1,7 @@
 package com.infosys.controller;
 
+import java.util.Calendar;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
@@ -12,12 +14,25 @@ import com.infosys.service.LowestTemperatureService;
 @RestController
 public class LowestTemperatureController {
 	@Autowired
-	LowestTemperatureService LowestTemperatureService;
+	LowestTemperatureService lowestTemperatureService;
 	
 	
 	@GetMapping(value = "/get/{zipCode}")
 	public String getTomorrowLowestTemperature(@PathVariable String zipCode) {
-		
+        String fileName = "./Weather.Service.Data.80421.xml";
+        
+        Calendar c = Calendar.getInstance();
+        //c.set(Calendar.DAY_OF_WEEK,7);
+        int dayOfWeek = c.get(Calendar.DAY_OF_WEEK);
+        int targetDayOfWeek = dayOfWeek==7?1:dayOfWeek+1;
+        
+        System.out.println("Day of week = "+dayOfWeek+" Target day of week "+targetDayOfWeek);
+        
+        
+        double lowestTemp = lowestTemperatureService.getLowestTemperature(fileName, targetDayOfWeek);
+        
+        System.out.println("Tomorrows lowest temp = "+lowestTemp);
+
 		
 		return "0";
 	}
--- src/main/java/com/infosys/service/LowestTemperatureService.java
+++ src/main/java/com/infosys/service/LowestTemperatureService.java
@@ -1,8 +1,68 @@
 package com.infosys.service;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.namespace.QName;
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.Attribute;
+import javax.xml.stream.events.EndElement;
+import javax.xml.stream.events.StartElement;
+import javax.xml.stream.events.XMLEvent;
+
 import org.springframework.stereotype.Service;
 
 @Service
-public class LowestTemperatureService {
+public class LowestTemperatureService {	
+    public double getLowestTemperature(String fileName, int targetDayOfWeek) {
+        List<Long> empList = new ArrayList<Long>();
+        int currentDayOfWeek = 0;
+        double lowestTemp = 200.0;
+        XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
+        try {
+            XMLEventReader xmlEventReader = xmlInputFactory.createXMLEventReader(new FileInputStream(fileName));
+            
+            while(xmlEventReader.hasNext()){
+                XMLEvent xmlEvent = xmlEventReader.nextEvent();
+               if (xmlEvent.isStartElement()){
+                   StartElement startElement = xmlEvent.asStartElement();
+                   //System.out.println("element is "+startElement.getName().getLocalPart());
+                   if(startElement.getName().getLocalPart().equals("forecast")){
+                       //Get the 'id' attribute from Employee element
+                       Attribute idAttr = startElement.getAttributeByName(new QName("dayOfWeek"));
+                       if(idAttr != null){
+                    	   currentDayOfWeek = Integer.parseInt(idAttr.getValue());
+                    	   
+                    	   //if(currentDayOfWeek == targetDayOfWeek)
+                    		//   System.out.println("value="+idAttr.getValue());
+                       }
+                   }
+                   /**
+                    * Get element temperature and compare to lowest variable.
+                    */
+                   if(startElement.getName().getLocalPart().equals("temperature")
+                		   &&(currentDayOfWeek == targetDayOfWeek)){
+                       xmlEvent = xmlEventReader.nextEvent();
+                       double thisTemp = Double.parseDouble(xmlEvent.asCharacters().getData());
+                       lowestTemp = Double.compare(thisTemp,lowestTemp)<0?thisTemp:lowestTemp;
+                   }
+               }
+               if(xmlEvent.isEndElement()){
+                   EndElement endElement = xmlEvent.asEndElement();
+                   if(endElement.getName().getLocalPart().equals("forecast")){
+                       currentDayOfWeek = 0;
+                   }
+               }
+            }
+            
+        } catch (FileNotFoundException | XMLStreamException e) {
+            e.printStackTrace();
+        }
+        return lowestTemp;
+    }
 
 }
